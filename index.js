82c14155f7f6813fcab82ef9d2226aa649e1cbb4
jaeyoung <seh0985@naver.com>
2021-06-25 16:18:07 +0900

    11st

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like babo!');
+    res.send('I like babo2!');
 });
 
 app.get('/start', function(req, res){
